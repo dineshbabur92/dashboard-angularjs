1485fe686764fbaaf54d878ef276cd0c64cd5efa
Ubuntu <ubuntu@ip-172-31-16-215.eu-central-1.compute.internal>
2017-11-15 12:33:45 +0000

    fixed disconnect

--- models.js
+++ models.js
@@ -3,14 +3,30 @@ var _ = require("underscore");
 
 module.exports = function(wagner){
 
-	var connection = mysql.createConnection({
-	  host     : '172.16.7.73', //'localhost',
-	  user     : 'root',
-	  password : 'dinesh',
-	  database : 'bmw'
-	});
-	 
-	connection.connect();
+	var connection;
+	console.log(process.env.MARIADB_IP);
+	function handleDisconnect() {
+
+		connection = mysql.createConnection({
+		  host     : process.env.MARIADB_IP, //'localhost',
+		  user     : 'root',
+		  password : 'dinesh',
+		  database : 'bmw'
+		});
+		 
+		connection.connect();
+	                                          
+		connection.on('error', function(err) {
+			console.log('db error', err);
+			if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			  handleDisconnect();                         // lost due to either server restart, or a
+			} else {                                      // connnection idle timeout (the wait_timeout
+			  throw err;                                  // server variable configures this)
+			}
+		});
+	}
+
+	handleDisconnect();
 	 
 	wagner.factory("db", function(){	
 		return connection;
